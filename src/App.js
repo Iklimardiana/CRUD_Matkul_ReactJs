858e70927fc2c2c21932c8790b39c43189829baf
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2023-06-27 08:48:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import FinalProject from './finalProject/finalProject';
+
+function App() {
+  return (
+    <FinalProject />
+  )
+}
+
+export default App;
